f8a5ad674240eb1c1a729e7dac70bcbe627a9956
atom <atom_1124@126.com>
2020-01-31 22:48:13 +0800

    web畅购10.0 待实现购物车功能

---
+++ changgou_parent_ee79/changgou_service_api/changgou_service_goods_api/src/main/java/com/changgou/goods/feign/SkuFeign.java
@@ -0,0 +1,33 @@
+package com.changgou.goods.feign;
+
+import com.changgou.goods.pojo.Sku;
+import entity.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @author Steven
+ * @version 1.0
+ * @description com.changgou.goods.feign
+ * @date 2019-12-25
+ */
+//FeignClient(name:微服务的名称)
+@FeignClient(name = "goods")
+@RequestMapping("/sku")
+public interface SkuFeign {
+    //查询所有匹配状态的sku列表
+    @GetMapping("/status/{status}")
+    public Result<List<Sku>> findByStatus(@PathVariable String status);
+
+    /***
+     * 根据ID查询Sku数据
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Result<Sku> findById(@PathVariable Long id);
+}
